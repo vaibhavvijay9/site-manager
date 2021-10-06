3142aceb77acfeb26b912992a99ec08e3e925a95
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2021-10-06 10:59:32 +0530

    implemented doughnut chart

--- src/app/components/doughnut-chart/doughnut-chart.component.ts
+++ src/app/components/doughnut-chart/doughnut-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'doughnut-chart',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoughnutChartComponent implements OnInit {
 
+  @Input() consumed: any = '0';
+  @Input() total: any = '0';
+  @Input() inputType: any;      // NUMBER, STRING
+
+  consumedCount: number = 0;
+  totalCount: number = 0;
+  percentage: any = '0%';
+
+  @ViewChild('radialProgress') radialProgress: any;
+
+
   constructor() { }
 
-  ngOnInit(): void {
+
+  ngOnInit(): void { 
+  }
+
+
+  ngAfterViewInit() {
+    if(this.inputType === 'STRING') {
+      this.consumedCount = (this.consumed).match(/\d+/)[0];
+      this.totalCount = (this.total).match(/\d+/)[0];
+      this.percentage = (this.consumedCount / this.totalCount)*100 + '%';
+    }
+    else {
+      this.percentage = (this.consumed / this.total)*100 + '%';
+    }
+    this.setProgress(this.percentage);
+  }
+
+  setProgress(progress: number) {
+    const value = `${progress}`;
+    this.radialProgress.nativeElement.style.setProperty('--progress', value)
+    this.radialProgress.nativeElement.innerHTML = this.consumed +'/'+ this.total;
+    this.radialProgress.nativeElement.setAttribute('aria-valuenow', value)
   }
 
 }
